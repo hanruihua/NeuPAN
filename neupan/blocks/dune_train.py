5559d13cf8b0e999df9b8b71ac517c15c612b37e
Han <744475118@qq.com>
2025-04-28 14:57:56 +0800

    [Enh] update file header

--- neupan/blocks/dune_train.py
+++ neupan/blocks/dune_train.py
@@ -4,18 +4,18 @@ DUNETrain is the class for training the DUNE model. It is used when you deploy t
 Developed by Ruihua Han
 Copyright (c) 2025 Ruihua Han <hanrh@connect.hku.hk>
 
-This program is free software: you can redistribute it and/or modify
+NeuPAN planner is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+NeuPAN planner is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
+along with NeuPAN planner. If not, see <https://www.gnu.org/licenses/>.
 """
 
 import torch
